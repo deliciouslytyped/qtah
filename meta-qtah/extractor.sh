235d39ee62a7584067fdda88a0dd97048c5db836
deliciouslytyped <deliciouslytyped>
2020-03-10 10:42:50 +0100

    fix qt include in extractor.sh

--- meta-qtah/extractor.sh
+++ meta-qtah/extractor.sh
@@ -4,7 +4,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 #export CLANG_INSTALL_DIR=/nix/store/d3gsn478zaxc7h4mlaadrig6cx36sk6i-clang-7.1.0
-qtIncludes=/nix/store/683fblqihpz1wbrhibwbp02aq684plm7-qtbase-5.12.7-dev/include
+qtIncludes=$(nix-build '<nixpkgs>' -A qt5.qtbase.dev)/include #TODO pin
 
 include=$1
 shift
