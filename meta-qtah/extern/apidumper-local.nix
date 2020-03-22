ce3a1a24cd4691f382f372b70938b4c4611f231f
name <email>
2020-03-22 19:33:43 +0100

    restructure directories and small cleanup

--- meta-qtah/extern/apidumper-local.nix
+++ meta-qtah/extern/apidumper-local.nix
@@ -1,7 +1,7 @@
 let
   # Look here for information about how to generate `nixpkgs-version.json`.
   #  → https://nixos.wiki/wiki/FAQ/Pinning_Nixpkgs
-  pinnedVersion = builtins.fromJSON (builtins.readFile ../upstream/nixpkgs-version.json);
+  pinnedVersion = builtins.fromJSON (builtins.readFile ./upstream/nixpkgs-version.json);
   pinnedPkgs = import (builtins.fetchGit {
     inherit (pinnedVersion) url rev;
 
